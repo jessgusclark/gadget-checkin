15292fc6e4a5865aab1e8b53e20ebf6aef5c6b8c
Jesse Clark <jesse.clark@unco.edu>
2016-03-14 11:12:55 -0600

    Merge changes into gh-pages.

--- build/js/gadget.js
+++ build/js/gadget.js
@@ -10457,7 +10457,7 @@ $(document).ready(function () {
         getListOfSites();
 
         // add class:
-        $("#main").addClass( getParameterByName("place", $(document).parent().context.URL) );
+        $("#main").addClass( gadget.get('place') );
 
     });
 
@@ -10544,18 +10544,6 @@ $(document).ready(function () {
             }
             
         });
-    }
-
-    // Source : http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
-    function getParameterByName(name, url) {
-        if (!url) url = window.location.href;
-        url = url.toLowerCase(); // This is just to avoid case sensitiveness  
-        name = name.replace(/[\[\]]/g, "\\$&").toLowerCase();// This is just to avoid case sensitiveness for query parameter name
-        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-            results = regex.exec(url);
-        if (!results) return null;
-        if (!results[2]) return '';
-        return decodeURIComponent(results[2].replace(/\+/g, " "));
     }
 
     // EVENT HANDLERS:
