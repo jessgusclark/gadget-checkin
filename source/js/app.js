bcc714b8850070e0e97b2bdc7a9f7358570df199
Jesse Clark <jesse.clark@unco.edu>
2016-03-14 10:52:00 -0600

    Add way to detect sidebar or dashboard and add the class to the main div.

--- source/js/app.js
+++ source/js/app.js
@@ -27,6 +27,9 @@ $(document).ready(function () {
         // do stuff...
         getListOfSites();
 
+        // add class:
+        $("#main").addClass( getParameterByName("place", $(document).parent().context.URL) );
+
     });
 
 
@@ -114,6 +117,17 @@ $(document).ready(function () {
         });
     }
 
+    // Source : http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+    function getParameterByName(name, url) {
+        if (!url) url = window.location.href;
+        url = url.toLowerCase(); // This is just to avoid case sensitiveness  
+        name = name.replace(/[\[\]]/g, "\\$&").toLowerCase();// This is just to avoid case sensitiveness for query parameter name
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
 
     // EVENT HANDLERS:
 
