80c6f0e7d65db0b90e359fe671e8a0394f955693
Jesse Clark <jesse.clark@unco.edu>
2016-03-17 08:40:48 -0600

    Move clearing the table to its own function and use remove() so the 'CheckIn all' remains.

--- source/js/app.js
+++ source/js/app.js
@@ -119,6 +119,11 @@ $(document).ready(function () {
         });
     }
 
+    function ClearList(){
+        $("table#checkedOut tr.site").remove();
+        checkedOutContent = [];
+    }
+
     // EVENT HANDLERS:
 
     $(document).on("click", ".check-in", function(){
@@ -134,8 +139,7 @@ $(document).ready(function () {
     });
 
     $("#refresh").click(function(){
-        $("table#checkedOut tbody").html(" ");
-        checkedOutContent = [];
+        ClearList();
         getListOfSites();
     });
 
